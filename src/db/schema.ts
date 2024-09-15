22f326fed26b5aa6fd5f87825388400d39e92a0a
gabriel ribeiro <gabrielsribeiro9999@gmail.com>
2024-09-15 16:51:38 -0300

    Finished

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -6,7 +6,7 @@ export const goals = pgTable('goals', {
     .primaryKey()
     .$defaultFn(() => createId()),
   title: text('title').notNull(),
-  desiredWeeklyFrequency: integer('desired_weekly_frequency').notNull(),
+  desiredWeeklyFrequency: integer('desired_week_frequency').notNull(),
   createdAt: timestamp('created_at', { withTimezone: true })
     .notNull()
     .defaultNow(),
